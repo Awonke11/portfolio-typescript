6c8d095edf703f010452830d5e4c2f66b48bd9b8
Awonke Mnotoza <93478189+Awonke11@users.noreply.github.com>
2022-11-29 12:05:06 +0200

    Open New Page

--- src/components/ProjectView.tsx
+++ src/components/ProjectView.tsx
@@ -18,15 +18,15 @@ const ProjectView = ({projectImage, figmaLink, liveLink, githubLink, title, desc
             <div className="project-view-content">
                 <img className="project-view-content-image" src={projectImage} alt="demo" width="200px" />
                 <div className="project-view-content-links">
-                    <a className="project-view-content-links-link" href={`${figmaLink}`}>
+                    <a className="project-view-content-links-link" href={`${figmaLink}`} target="_blank" rel="noopener noreferrer">
                         <FaFigma className="project-view-content-links-link-icon" />
                         <h4 className="project-view-content-links-link-title">Figma</h4>
                     </a>
-                    <a className="project-view-content-links-link" href={`${liveLink}`}>
+                    <a className="project-view-content-links-link" href={`${liveLink}`} target="_blank" rel="noopener noreferrer">
                         <FaEye className="project-view-content-links-link-icon" />
                         <h4>Live</h4>
                     </a>
-                    <a className="project-view-content-links-link" href={`${githubLink}`}>
+                    <a className="project-view-content-links-link" href={`${githubLink}`} target="_blank" rel="noopener noreferrer">
                         <FaGithub className="project-view-content-links-link-icon" />
                         <h4 className="project-view-content-links-link-title">Code</h4>
                     </a>
